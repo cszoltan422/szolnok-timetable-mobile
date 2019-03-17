90d0b692f197d6a67834cc1b0b4a7c1e68285be9
Zoltan Csenyi <cszoltan422@gmail.com>
2019-03-17 21:14:19 +0100

    Add splash screen and resource files

---
+++ app/src/main/java/org/zenbot/szolnok/timetable/SplashScreenActivity.java
@@ -0,0 +1,27 @@
+package org.zenbot.szolnok.timetable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        int delay = getResources().getInteger(R.integer.splash_screen_delay_in_millis);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                SplashScreenActivity.this.startActivity(intent);
+                SplashScreenActivity.this.finish();
+            }
+        }, delay);
+    }
+}
